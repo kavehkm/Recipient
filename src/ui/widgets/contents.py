0ea6e41e7530c5a9f4987fd273f28b6f4a32ff6a
kavehkm <mehrbaniankaveh@gmail.com>
2021-05-19 06:50:14 +0430

    create contents tab: status

--- src/ui/widgets/contents.py
+++ src/ui/widgets/contents.py
@@ -1,5 +1,5 @@
 # pyqt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QFrame, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
 
 
 class BaseTab(QWidget):
@@ -7,20 +7,94 @@ class BaseTab(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupTab()
+        self.setStyles()
 
     def setupTab(self):
         self.generalLayout = QVBoxLayout()
         self.setLayout(self.generalLayout)
         self.generalLayout.setContentsMargins(0, 0, 0, 0)
 
+    def setStyles(self):
+        pass
+
 
 class StatusTab(BaseTab):
     """Status Tab"""
+    def __init__(self):
+        super().__init__()
+        # default state: stopped
+        self.stop()
+
     def setupTab(self):
         super().setupTab()
-        self.lbl = QLabel()
-        self.lbl.setText('<h3>Status Tab</h3>')
-        self.generalLayout.addWidget(self.lbl)
+        # service frame
+        self.serviceFrame = QFrame()
+        self.serviceFrame.setObjectName('ServiceFrame')
+        self.generalLayout.addWidget(self.serviceFrame)
+        # service layout
+        self.serviceLayout = QVBoxLayout()
+        self.serviceFrame.setLayout(self.serviceLayout)
+        # service name
+        self.serviceName = QLabel('<h2>Recipient Engine</h2>')
+        self.serviceName.setFixedHeight(60)
+        self.serviceLayout.addWidget(self.serviceName)
+        # service state
+        serviceStateLayout = QHBoxLayout()
+        serviceStateLayout.setContentsMargins(0, 10, 0, 10)
+        self.serviceStateLabel = QLabel('<b>status: </b>')
+        self.serviceStateVal = QLabel()
+        serviceStateLayout.addWidget(self.serviceStateLabel)
+        serviceStateLayout.addWidget(self.serviceStateVal)
+        serviceStateLayout.addStretch(1)
+        self.serviceLayout.addLayout(serviceStateLayout)
+        # service control
+        serviceControlLayout = QHBoxLayout()
+        self.serviceStart = QPushButton('start')
+        self.serviceStop = QPushButton('stop')
+        serviceControlLayout.addWidget(self.serviceStart)
+        serviceControlLayout.addWidget(self.serviceStop)
+        serviceControlLayout.addStretch(1)
+        self.serviceLayout.addLayout(serviceControlLayout)
+        # add stretch at the end
+        self.generalLayout.addStretch(1)
+
+    def setStyles(self):
+        self.setStyleSheet("""
+            #ServiceFrame{
+                border: 1px solid silver;
+                border-right-width: 15px;
+            }
+            #ServiceFrame[state="start"]{
+                border-right-color: #26d926;
+            }
+            #ServiceFrame[state="stop"]{
+                border-right-color: red;
+            }
+            #ServiceFrame[state="connecting"]{
+                border-right-color: orange;
+            }
+        """)
+
+    def start(self):
+        self.serviceStateVal.setText('running')
+        self.serviceStart.setDisabled(True)
+        self.serviceStop.setEnabled(True)
+        self.serviceFrame.setProperty('state', 'start')
+        self.serviceFrame.setStyleSheet(self.serviceFrame.styleSheet())
+
+    def stop(self):
+        self.serviceStateVal.setText('stopped')
+        self.serviceStart.setEnabled(True)
+        self.serviceStop.setDisabled(True)
+        self.serviceFrame.setProperty('state', 'stop')
+        self.serviceFrame.setStyleSheet(self.serviceFrame.styleSheet())
+
+    def connecting(self):
+        self.serviceStateVal.setText('connecting')
+        self.serviceStart.setDisabled(True)
+        self.serviceStop.setDisabled(True)
+        self.serviceFrame.setProperty('state', 'connecting')
+        self.serviceFrame.setStyleSheet(self.serviceFrame.styleSheet())
 
 
 class InvoicesTab(BaseTab):
