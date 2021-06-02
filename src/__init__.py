5291c1bb610968411da9ac99a9dad98065efb193
kavehkm <mehrbaniankaveh@gmail.com>
2021-06-02 16:41:15 +0430

    1) add some ui alias for better references 2) connect updateWP signals to controller handlers

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
+# standard
+import random
+from datetime import datetime
 # internal
 from src import settings as s
 
@@ -6,6 +9,15 @@ class Controller(object):
     """Controller"""
     def __init__(self, ui):
         self.ui = ui
+        # ui alias
+        self.ui_status = self.ui.contents.status
+        self.ui_invoices = self.ui.contents.invoices
+        self.ui_updateWP = self.ui.contents.updateWP
+        self.ui_settings = self.ui.contents.settings
+        self.ui_logs = self.ui.contents.logs
+        self.ui_help = self.ui.contents.help
+        self.ui_about = self.ui.contents.about
+        # bootstrap
         self.bootstrap()
 
     def bootstrap(self):
@@ -21,14 +33,14 @@ class Controller(object):
         self.ui.menu.btnHelp.clicked.connect(self.help_handler)
         self.ui.menu.btnAbout.clicked.connect(self.about_handler)
         # contents: status tab
-        self.ui.contents.status.btnStart.clicked.connect(self.status_btnStart_handler)
-        self.ui.contents.status.btnStop.clicked.connect(self.status_btnStop_handler)
+        self.ui_status.btnStart.clicked.connect(self.status_btnStart_handler)
+        self.ui_status.btnStop.clicked.connect(self.status_btnStop_handler)
         # contents: settings tab
-        self.ui.contents.settings.btnSave.clicked.connect(self.settings_btnSave_handler)
-        self.ui.contents.settings.btnClear.clicked.connect(self.settings_btnClear_handler)
+        self.ui_settings.btnSave.clicked.connect(self.settings_btnSave_handler)
+        self.ui_settings.btnClear.clicked.connect(self.settings_btnClear_handler)
         # contents: updateWP tab
-        self.ui.contents.updateWP.tabs.currentChanged.connect(self.updateWP_tabs_handler)
-        self.ui.contents.updateWP.signalAction.connect(self.updateWP_action_dispatcher)
+        self.ui_updateWP.tabs.currentChanged.connect(self.updateWP_tabs_handler)
+        self.ui_updateWP.signalAction.connect(self.updateWP_action_dispatcher)
 
     ###################
     # status handlers #
@@ -37,10 +49,10 @@ class Controller(object):
         self.ui.contents.showTab(self.ui.contents.STATUS)
 
     def status_btnStart_handler(self):
-        self.ui.contents.status.start()
+        self.ui_status.start()
 
     def status_btnStop_handler(self):
-        self.ui.contents.status.stop()
+        self.ui_status.stop()
 
     #####################
     # invoices handlers #
@@ -52,55 +64,110 @@ class Controller(object):
     # updateWP handlers #
     #####################
     def updateWP_handler(self):
-        current_tab = self.ui.contents.updateWP.tabs.currentIndex()
+        current_tab = self.ui_updateWP.tabs.currentIndex()
         self.updateWP_tabs_handler(current_tab)
         self.ui.contents.showTab(self.ui.contents.UPDATE_WP)
 
     def updateWP_tabs_handler(self, index):
-        print(index)
+        if index == self.ui_updateWP.CATEGORIES:
+            self.updateWP_init_registered_categories()
+        elif index == self.ui_updateWP.PRODUCTS:
+            self.updateWP_init_registered_products()
+
+    def updateWP_init_registered_categories(self):
+        # dummy data
+        categories = []
+        for i in range(5):
+            random_int = random.randint(1, 10000)
+            category = [
+                random_int,
+                f'Category{random_int}',
+                random_int * 2,
+                datetime.now().strftime('%Y/%m/%d')
+            ]
+            categories.append(category)
+        self.ui_updateWP.registeredCategoryTable.setRecords(categories)
+
+    def updateWP_init_registered_products(self):
+        # dummy data
+        products = []
+        for i in range(50):
+            random_int = random.randint(1, 10000)
+            product = [
+                random_int,
+                random_int + 1,
+                f'Product{random_int}',
+                random_int * 2,
+                datetime.now().strftime('%Y/%m/%d')
+            ]
+            products.append(product)
+        self.ui_updateWP.registeredProductsTable.setRecords(products)
 
     def updateWP_action_dispatcher(self, tab, action):
-        # create alias
-        update_wp = self.ui.contents.updateWP
-        if tab == update_wp.CATEGORIES:
-            if action == update_wp.ADD:
+        if tab == self.ui_updateWP.CATEGORIES:
+            if action == self.ui_updateWP.ADD:
                 self.updateWP_add_category()
-            elif action == update_wp.EDIT:
+            elif action == self.ui_updateWP.EDIT:
                 self.updateWP_edit_category()
-            elif action == update_wp.REMOVE:
+            elif action == self.ui_updateWP.REMOVE:
                 self.updateWP_remove_category()
-            elif action == update_wp.UPDATE:
+            elif action == self.ui_updateWP.UPDATE:
                 self.updateWP_update_categories()
-        elif tab == update_wp.PRODUCTS:
-            if action == update_wp.ADD:
+        elif tab == self.ui_updateWP.PRODUCTS:
+            if action == self.ui_updateWP.ADD:
                 self.updateWP_add_product()
-            elif action == update_wp.EDIT:
+            elif action == self.ui_updateWP.EDIT:
                 self.updateWP_edit_product()
-            elif action == update_wp.REMOVE:
+            elif action == self.ui_updateWP.REMOVE:
                 self.updateWP_remove_product()
-            elif action == update_wp.UPDATE:
+            elif action == self.ui_updateWP.UPDATE:
                 self.updateWP_update_products()
 
     def updateWP_add_category(self):
-        print('add new category')
+        new_category = [
+            random.randint(1, 10000),
+            f'Category{random.randint(1, 10000)}',
+            random.randint(1, 10000),
+            datetime.now().strftime('%Y/%m/%d')
+        ]
+        self.ui_updateWP.registeredCategoryTable.addRecord(new_category)
 
     def updateWP_edit_category(self):
-        print('edit category')
+        category_index = self.ui_updateWP.registeredCategoryTable.getCurrentRecordIndex()
+        if category_index is not None:
+            category = self.ui_updateWP.registeredCategoryTable.getRecord(category_index)
+            category[1] = category[1] + '-edited'
+            self.ui_updateWP.registeredCategoryTable.updateRecord(category_index, category)
 
     def updateWP_remove_category(self):
-        print('remove category')
+        category_index = self.ui_updateWP.registeredCategoryTable.getCurrentRecordIndex()
+        if category_index is not None:
+            self.ui_updateWP.registeredCategoryTable.removeRecord(category_index)
 
     def updateWP_update_categories(self):
         print('update categories on wordpress')
 
     def updateWP_add_product(self):
-        print('add new product')
+        new_product = [
+            random.randint(1, 10000),
+            random.randint(1, 10000),
+            f'Product{random.randint(1, 10000)}',
+            random.randint(1, 10000),
+            datetime.now().strftime('%Y/%m/%d')
+        ]
+        self.ui_updateWP.registeredProductsTable.addRecord(new_product)
 
     def updateWP_edit_product(self):
-        print('edit product')
+        product_index = self.ui_updateWP.registeredProductsTable.getCurrentRecordIndex()
+        if product_index is not None:
+            product = self.ui_updateWP.registeredProductsTable.getRecord(product_index)
+            product[2] = product[2] + '-edited'
+            self.ui_updateWP.registeredProductsTable.updateRecord(product_index, product)
 
     def updateWP_remove_product(self):
-        print('remove product')
+        product_index = self.ui_updateWP.registeredProductsTable.getCurrentRecordIndex()
+        if product_index is not None:
+            self.ui_updateWP.registeredProductsTable.removeRecord(product_index)
 
     def updateWP_update_products(self):
         print('update products on wordpress')
@@ -109,20 +176,20 @@ class Controller(object):
     # settings handlers #
     #####################
     def settings_handler(self):
-        self.ui.contents.settings.set({
+        self.ui_settings.set({
             'wc': s.get('wc', {}),
             'moein': s.get('moein', {})
         })
         self.ui.contents.showTab(self.ui.contents.SETTINGS)
 
     def settings_btnSave_handler(self):
-        settings = self.ui.contents.settings.get()
+        settings = self.ui_settings.get()
         s.set('wc', settings.get('wc'))
         s.set('moein', settings.get('moein'))
         s.save()
 
     def settings_btnClear_handler(self):
-        self.ui.contents.settings.clear()
+        self.ui_settings.clear()
 
     #################
     # logs handlers #
