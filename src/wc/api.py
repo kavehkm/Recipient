baa3c713e91b3280b495788cc032056f7ecb7e99
kavehkm <mehrbaniankaveh@gmail.com>
2021-06-28 16:16:27 +0430

    1) install woocommerce and update requirements.txt 2) create wc package with base_model and api modules 3) complete api.py module and test with some unittest

---
+++ src/wc/api.py
@@ -0,0 +1,13 @@
+# internal
+from src import settings as s
+# woocommerce
+from woocommerce import API
+
+
+def get(url=None, ckey=None, skey=None, version=None, wp_api=True):
+    wc_settings = s.get('wc', {})
+    url = url or wc_settings.get('url')
+    ckey = ckey or wc_settings.get('ckey')
+    skey = skey or wc_settings.get('skey')
+    version = version or wc_settings.get('version')
+    return API(url, ckey, skey, wp_api=wp_api, version=version)
---
+++ tests/wc/tests.py
@@ -0,0 +1,35 @@
+# standard
+import unittest
+# internal
+from src.wc import api
+from src import settings
+
+
+class StatusCodes(object):
+    """Status Codes Collection"""
+    Success =       200     # success
+    BadRequest =    400     # invalid request, e.g. using unsupported HTTP method
+    Unauthorized =  401     # authentication or permission error, e.g. incorrect API keys
+    NotFound =      404     # request to resources that dont exist or are missing
+    InternalError = 500     # server error
+
+
+class TestAPI(unittest.TestCase):
+    """Test API module"""
+    STATUS_CODES = StatusCodes()
+
+    def test_connection(self):
+        wcapi = api.get(**settings.get('wc'))
+        response = wcapi.get("")
+        self.assertEqual(response.status_code, self.STATUS_CODES.Success)
+
+    def test_invalid_connection(self):
+        invalid_api = {
+            'url': 'http://localhost/Store',
+            'ckey': 'ck_nul1sbjblc9oc4lt8izq26hkwcib9vhxtx88an6e',
+            'skey': 'cs_hwf9fvtntbu5ek105oez1obg99v5zijb6q5kcfqd',
+            'version': 'wc/v3'
+        }
+        wcapi = api.get(**invalid_api)
+        response = wcapi.get("")
+        self.assertEqual(response.status_code, self.STATUS_CODES.Unauthorized)
