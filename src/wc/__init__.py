5e464b68ae2bea4a30cdf2e8d88ab355f57062e9
kavehkm <mehrbaniankaveh@gmail.com>
2021-07-02 13:24:13 -0700

    extends create methods on wc models(Category, Product)

--- src/wc/__init__.py
+++ src/wc/__init__.py
@@ -5,7 +5,24 @@ class Category(WCBaseModel):
     """WooCommerce Category Model"""
     ENDPOINT = 'products/categories'
 
+    def create(self, name, slug, parent=0):
+        data = {
+            'name': name,
+            'slug': slug,
+            'parent': parent
+        }
+        return super().create(data)
+
 
 class Product(WCBaseModel):
     """WooCommerce Product Model"""
     ENDPOINT = 'products'
+
+    def create(self, name, slug, regular_price, parent_id=0):
+        data = {
+            'name': name,
+            'slug': slug,
+            'parent_id': parent_id,
+            'regular_price': regular_price
+        }
+        return super().create(data)
