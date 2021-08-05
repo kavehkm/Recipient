dba1e535b30e53cef3390c8dcb383f7590594a92
kavehkm <mehrbaniankaveh@gmail.com>
2021-08-05 03:01:34 -0700

    create RecipientEngineSignals QObject to collect needed signals in RecipientEngine, add pause and resume to Engine and add Engine error handling method into StatusView

--- src/views/status.py
+++ src/views/status.py
@@ -2,8 +2,15 @@
 import time
 import random
 from threading import Event
+# internal
+from src.ui.components import Message
 # pyqt
-from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QThread, QObject, pyqtSignal
+
+
+class RecipientEngineSignals(QObject):
+    """Recipient Engine Signals"""
+    error = pyqtSignal(object)
 
 
 class RecipientEngine(QThread):
@@ -11,22 +18,39 @@ class RecipientEngine(QThread):
     def __init__(self, interval):
         super().__init__()
         self._interval = interval
-        # stop event
+        # events
+        # - stop
         self.stop_event = Event()
-
-    def stop(self):
-        # set stop event
-        self.stop_event.set()
-        self.quit()
+        # - resume
+        self.resume_event = Event()
+        self.resume_event.set()
+        # signals
+        self.signals = RecipientEngineSignals()
 
     def start(self):
         # clear stop event
         self.stop_event.clear()
         super().start()
 
+    def pause(self):
+        self.resume_event.clear()
+
+    def resume(self):
+        self.resume_event.set()
+
+    def stop(self):
+        # set resume event
+        self.resume_event.set()
+        # set stop event
+        self.stop_event.set()
+        self.quit()
+
     def _do(self):
         time.sleep(2)
-        print(random.randint(1, 1000))
+        random_int = random.randint(1, 1000)
+        print(random_int)
+        if random_int > 700:
+            raise Exception('some many tears')
 
     def run(self):
         # do-while(stop_event is not set)
@@ -34,10 +58,13 @@ class RecipientEngine(QThread):
             try:
                 self._do()
             except Exception as e:
-                pass
-            else:
-                if self.stop_event.wait(self._interval):
-                    break
+                self.pause()
+                self.signals.error.emit(e)
+                # check for pause event
+            self.resume_event.wait()
+            # check for stop event
+            if self.stop_event.wait(self._interval):
+                break
 
 
 class Status(object):
@@ -58,6 +85,7 @@ class Status(object):
         # - engine
         self.engine.started.connect(self.tab.start)
         self.engine.finished.connect(self.tab.stop)
+        self.engine.signals.error.connect(self.engine_error_handle)
 
     def tab_handler(self):
         self.ui.contents.showTab(self.ui.contents.STATUS)
@@ -66,4 +94,9 @@ class Status(object):
         self.engine.start()
 
     def stop(self):
+        self.engine.stop()
+
+    def engine_error_handle(self, error):
+        msg = Message(self.ui, Message.ERROR, 'Error', str(error))
+        msg.show()
         self.engine.stop()
